41aa911768ffdf5d3cee7ec13b5430e54275a062
Parrot User <user@localhost.localdomain>
2020-03-21 02:05:48 -0400

    Included comments

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 #My First Python Project
 my python project change my mac_address
-by following the hacking course by zaid sabith
+by following the hacking course by zaid sabih
+
+to run this program navigate to the project root folder and type
+$ python mac_changer.py -i eth0 -m 00:11:22:33:44:11
+
--- mac_changer.py
+++ mac_changer.py
@@ -4,11 +4,19 @@ import subprocess
 import optparse 
 import re
 
+#this function allows us to acquire the Mac address and network interface (wifi card ethernet card) values from the Terminal, theses values are input by the user
 def get_arguments():
+    #for this we use the optparse module
      parser = optparse.OptionParser()
+     #get the network interface device (WIFI card ethernet card)
      parser.add_option("-i", "--interface", dest="interface",  help="Interface to change its MAC address")
+     #get the new MAC address
      parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+     
+     #get the mac address and interface values
      (options, arguments) = parser.parse_args()
+     
+     #if no values were entered then show error messages
      if not options.interface:
             parser.error("[-] Please Specify an interface, use --help for more info.")
      elif not options.new_mac:
@@ -16,15 +24,22 @@ def get_arguments():
      return options
        
 
+#the function below changes the MAC address of the specified interface
 def change_mac (interface, new_mac):
      print ("[+] Changing MAC address for " + interface + " to " + new_mac)
+     #turn down the interface
      subprocess.call(["ifconfig", interface, "down"])
+     #set the New mac address for the interface
      subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+     #turn the interface up
      subprocess.call(["ifconfig", interface, "up"])
 
+#this function checks if the new mac address was set properly
 def get_current_mac(interface):
       ifconfig_result = subprocess.check_output (["ifconfig", interface])
+      #regular expression to read mac address from string
       mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w:", ifconfig_result)
+     
       if mac_address_search_result: 
              return mac_address_search_result.group(0)
       else:
@@ -34,12 +49,18 @@ options = get_arguments()
 current_mac = get_current_mac(options.interface)
 print("Current MAC = " + str(current_mac))
 
-change_mac(options.interface, options.new_mac)
-
 current_mac = get_current_mac(options.interface)
 if current_mac == options.new_mac: 
        print("[+] MAC address was sucessfully changed to " + current_mac)
 else:
        print("[-] MAC address did not got changed.")
+       
+change_mac(options.interface, options.new_mac)
 
+current_mac = get_current_mac(options.interface) 
+if current_mac == options.new_mac: 
+    print("[+] MAC address successfully changed to " + current_mac)
+    
+else: 
+    print("[-] MAC address did not get changed.")
 
